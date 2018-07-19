9ce72fb6f2e6980c0fbe7ad208c01177784bd010
Luke Olali <luke.olali@andela.com>
2018-07-19 09:52:21 +0300

    Removes commented out code used for debugging purposes

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,31 +14,18 @@ const getChannelID = async (name) => {
 }
 
 router.post('/staffed/:channel/:email',  (req, res) => {
-  // const emailPrefix = req.params.email;
-  // const emailSuffix = '@andela.com';
-  // const email = emailPrefix + emailSuffix;
   const channel = req.params.channel;
   const email = req.params.email;
 
   web.users.lookupByEmail({ token: process.env.SLACK_TOKEN, email: email })
   .then(async (result) => {
     const channel_id = await getChannelID(channel);
-    // console.log(channel_id);
-    // res.send({
-    //   slack_handle: result.user.profile.display_name,
-    //   real_name: result.user.real_name,
-    //   channel_id: channel_id,
-    //   user_id: result.user.id
-    // });
-
     web.groups.invite({
       token: process.env.SLACK_TOKEN,
       channel: channel_id,
       user: result.user.id
     })
     .then(arg => {
-      // console.log('User invited to channel!');
-      // console.log(arg)
       res.send({
         invitation: result.user.real_name,
         channel: channel_id,
