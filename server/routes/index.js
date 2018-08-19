3ba38bbef4d452466a88b0f7d2b6d1df597b9819
Luke Olali <luke.olali@andela.com>
2018-08-19 20:52:46 +0300

    - Refactors the getChannelID() function to make use of promises and handle edge cases - Changes the request format from GET to POST for both staffing and rolloff request - Creates staffing_slack_operation() function that handles inviting or removing user(s) from slack   channels concurrently during a staffing request - Creates rolloff_slack_operation() function that handles removing or inviting user(s) to slack   channels concurrently during a rolloff request - Creates searchByEmail() function that enables user(s) passed in during a request to be looked up   using their respective emails - Checks if partner channel exists before proceeding with either a rolloff or staffing request - Ensures that all user email(s) provided in a request actually do exist in the slack workspace

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,117 +2,101 @@ require('dotenv').config();
 
 const express = require('express');
 const router = express.Router();
-const { WebClient } = require('@slack/client');
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
-const core_channels = require('./core_channels');
 
-const web = new WebClient(SLACK_TOKEN);
+import { 
+  getChannelID, 
+  searchUserByEmail, 
+  staffing_slack_operation,
+  rolloff_slack_operation,
+} from './router_utils';
 
-import { getChannelID, invite_to_channel, remove_from_channel } from './router_utils';
+router.post('/staffed', async (req, res) => {
 
-router.get('/staffed/:channel/:email', async (req, res) => {
-  const channel_name = req.params.channel;
-  const email = req.params.email;
+  if (!req.body.emails instanceof Array) {
+    req.body.emails = new Array(req.body.email);
+  }
 
-  web.users.lookupByEmail({
-      token: SLACK_TOKEN,
-      email: email
-    })
-    .then(async (result) => {
+  const channel_name = req.body.channel;
+  const user_emails = req.body.emails;
 
-      try {
-        // proceed only if the channel sent using the request exists
-        var partner_channel_id = await getChannelID(SLACK_TOKEN, channel_name);
-      } catch (err) {
-        const err = new Error('the partner channel does not exist');
-        res.status(500).send({
-          error: "The partner channel doesn't exist"
-        });
-      }
+  try {
+    const channel_id = await getChannelID(SLACK_TOKEN, channel_name)
+    if (channel_id instanceof Error) {
+      throw new Error(`API Error`)
+    }
 
-      // const partner_channel_id = await getChannelID(channel_name);
-
-      const available_devs_channel_id = await getChannelID(SLACK_TOKEN, core_channels.AVAILABLE_DEVS);
-      const placed_ops_channel_id = await getChannelID(SLACK_TOKEN, core_channels.PLACED_OPS);
-      const rack_city_channel_id = await getChannelID(SLACK_TOKEN, core_channels.RACK_CITY);
-
-      try {
-        const response = await Promise.all(
-          [
-            // Kick of user from available-devs channel
-            remove_from_channel(SLACK_TOKEN, result, available_devs_channel_id),
-
-            // Invite user to p-partner channel
-            invite_to_channel(SLACK_TOKEN, result, partner_channel_id),
-
-            // Invite user to rack-city
-            invite_to_channel(SLACK_TOKEN, result, rack_city_channel_id),
-
-            // Invite to placed-fellows-ops
-            invite_to_channel(SLACK_TOKEN, result, placed_ops_channel_id)
-          ]
-        ).then(result => {
-          console.log(result)
-          res.send({ success: "User invited to and removed from all required channels" });
+    Promise.all(
+      user_emails.map(async (this_email) => {
+        return searchUserByEmail(this_email, SLACK_TOKEN)
+      })
+    ).then(values => {
+      // res.send({ data: values }) 
+      Promise.all(
+        values.map(async (this_user) => {
+          return staffing_slack_operation(SLACK_TOKEN, this_user, channel_name)
         })
-      } catch (err) {
-        console.log("error thrown")
-        console.log(err)
-        res.status(500).send({ error: "An err occurred while removing and inviting user to channels" });
+      ).then(values => {
+        // res.send({ values: values });
+        res.send({ success: "Operational completed successfully for all the specified users"})
         return
-      }
-
-    }).catch(error => {
-      res.status(404).send({ error: "Could not find user specified by the email"})
-    });
+      }).catch(err => {
+        res.send({ error: err.message + ' : Error while removing or adding user(s) to channels' })
+        return
+      })
+    }).catch(err => {
+      res.send({ error: err.message });
+      return;
+    })
+  } catch(err) {
+    res.send({ error: err.message + ` : The channel ${channel_name} does not exist`});
+    return;
+  }
+  
 });
 
-router.get('/rolloff/:channel/:email', (req, res) => {
-  const channel_name = req.params.channel;
-  const email = req.params.email;
+router.post('/rolloff', async (req, res) => {
 
-  web.users.lookupByEmail({ token: SLACK_TOKEN, email: email })
-    .then(async (result) => {
+  if (!req.body.email instanceof Array) {
+    req.body.email = new Array(req.body.email);
+  }
 
-      try {
-        // proceed only if the channel sent using the request exists
-        var partner_channel_id = await getChannelID(SLACK_TOKEN, channel_name);
-      } catch (err) {
-        const err = new Error('the partner channel does not exist');
-        res.status(500).send({
-          error: "The partner channel doesn't exist"
-        });
-      }
+  const channel_name = req.body.channel;
+  const user_emails = req.body.email;
 
-      // Get channel IDs to add to or remove user from
-      const available_devs_channel_id = await getChannelID(SLACK_TOKEN, core_channels.AVAILABLE_DEVS);
+  try {
+    const channel_id = await getChannelID(SLACK_TOKEN, channel_name)
+    if (channel_id instanceof Error) {
+      throw new Error(`API Error`)
+    }
 
-      try {
-        const response = await Promise.all(
-          [
-            // Kick of user from p-partner channel
-            remove_from_channel(SLACK_TOKEN, result, partner_channel_id),
-
-            // Invite user to available-devs channel
-            invite_to_channel(SLACK_TOKEN, result, available_devs_channel_id),
-          ]
-        ).then(result => {
-          console.log(result)
-          res.send({ success: "User invited to and removed from all required channels" });
-        })
-      } catch (err) {
-        console.log("error thrown")
-        console.log(err)
-        res.status(500).send({ error: "An err occurred while removing and inviting user to channels" });
-        return
-      }
-
-    })
-    .catch(error => {
-      res.status(404).send({
-        error: "Could not find the user specified by the email"
+    Promise.all(
+      user_emails.map(async (this_email) => {
+        return searchUserByEmail(this_email, SLACK_TOKEN)
       })
-    });
+    ).then(values => {
+      // res.send({ data: values }) 
+      Promise.all(
+        values.map(async (this_user) => {
+          return rolloff_slack_operation(SLACK_TOKEN, this_user, channel_name)
+        })
+      ).then(values => {
+        // res.send({ values: values });
+        res.send({ success: "Operational completed successfully for all the specified users"})
+        return
+      }).catch(err => {
+        res.send({ error: err.message + ' : Error while removing or adding user(s) to channels' })
+        return
+      })
+    }).catch(err => {
+      res.send({ error: err.message });
+      return;
+    })
+  } catch(err) {
+    res.send({ error: err.message + ` : The channel ${channel_name} does not exist`});
+    return;
+  }
+
 });
 
 module.exports = router;
--- server/routes/router_utils.js
+++ server/routes/router_utils.js
@@ -1,14 +1,25 @@
 const { WebClient } = require('@slack/client');
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 const web = new WebClient(SLACK_TOKEN);
+const core_channels = require('./core_channels');
 
 const getChannelID = async (slack_token, name) => {
-  const group_list = await web.groups.list({
-    token: slack_token
-  });
-  const target_group = group_list.groups.filter(g => g.name == name);
-  const group_id = target_group[0].id;
-  return group_id;
+  try {
+    const group_list = await web.groups.list({
+      token: slack_token
+    });
+    // return group_list;
+    const group_names = {}
+    group_list.groups.forEach(this_group => group_names[this_group.name] = this_group.id);
+    if (group_names.hasOwnProperty(name)) {
+      return new Promise((resolve) => resolve(group_names[name]))
+    } 
+    else {
+      return new Promise((reject) => reject(new Error(`The channel ${name} does not exist`)));
+    }   
+  } catch (err) {
+    return(err.message); 
+  } 
 }
 
 const userExists = async (slack_token, user_id, channel_id) => {
@@ -43,4 +54,56 @@ const invite_to_channel = async (slack_token, user_info_result_obj, channel_id) 
   return
 }
 
-export { getChannelID, invite_to_channel, remove_from_channel };
+const searchUserByEmail = (email, slack_token) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const user =  web.users.lookupByEmail({ token: slack_token, email: email });
+      resolve(user);
+    } catch (err) {
+      reject(err);
+    }
+  })
+}
+
+const staffing_slack_operation = async (slack_token, user_info_result_obj, partner_channel_name) => {
+  // Get channels to add user to or remove them from
+  const partner_channel_id = await getChannelID(slack_token, partner_channel_name);
+  const available_devs_channel_id = await getChannelID(slack_token, core_channels.AVAILABLE_DEVS);
+  const placed_ops_channel_id = await getChannelID(SLACK_TOKEN, core_channels.PLACED_OPS);
+  const rack_city_channel_id = await getChannelID(SLACK_TOKEN, core_channels.RACK_CITY);
+
+  return Promise.all([
+    // Kick of user from available-devs channel
+    remove_from_channel(slack_token, user_info_result_obj, available_devs_channel_id),
+
+    // Invite user to p-partner channel
+    invite_to_channel(slack_token, user_info_result_obj, partner_channel_id),
+
+    // Invite user to rack-city
+    invite_to_channel(SLACK_TOKEN, user_info_result_obj, rack_city_channel_id),
+
+    // Invite to placed-fellows-ops
+    invite_to_channel(SLACK_TOKEN, user_info_result_obj, placed_ops_channel_id)
+  ])
+}
+
+const rolloff_slack_operation = async (slack_token, user_info_result_obj, partner_channel_name) => {
+  // Get channels to add user to or remove them from
+  const partner_channel_id = await getChannelID(slack_token, partner_channel_name);
+  const available_devs_channel_id = await getChannelID(slack_token, core_channels.AVAILABLE_DEVS);
+
+  return Promise.all([
+    // Kick of user from p-partner channel
+    remove_from_channel(slack_token, user_info_result_obj, partner_channel_id),
+
+    // Invite user to available-devs channel
+    invite_to_channel(slack_token, user_info_result_obj, available_devs_channel_id)
+  ])
+}
+
+export { 
+  getChannelID, 
+  searchUserByEmail, 
+  staffing_slack_operation, 
+  rolloff_slack_operation
+};
