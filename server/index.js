a756c740c57b5c1dd0b073a96650a835a2d28b8f
Dominic Motuka <dominic.motuka@gmail.com>
2018-09-19 21:20:58 +0300

    Add and user swagger UI to serve docs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConfig from '../docs/swagger';
 
 import models from './models';
 
@@ -12,6 +14,8 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
