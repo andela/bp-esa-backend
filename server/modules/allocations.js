b21815e3e3ecc1a12444926d6f853d5f85a6a7a9
KC <Kelechukwu.nwosu@viacommix.com>
2018-10-11 00:11:43 +0100

    addded allocations module with contains function neccessary for integration with allocations, also setup redis store for partner list to avoid making unnessary requests to allocations

---
+++ server/modules/allocations.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import redis from 'redis';
+
+dotenv.config();
+
+// redis setup
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+client.on('connect', () => {
+  console.log('Redis client connected');
+});
+client.on('error', (err) => {
+  console.log(`Something went wrong ${err}`);
+});
+
+// axios base URL and Authorization header setup
+axios.defaults.baseURL = 'https://api.andela.com/';
+axios.defaults.headers.common = { Authorization: `bearer ${process.env.ANDELA_USER_JWT}` };
+
+export const placementStatus = {
+  rollingOff: 'External Engagements - Rolling Off',
+  onboarding: 'Placed - Awaiting Onboarding',
+};
+
+// Updates the local redis store with latest Partner List
+export const updatePartnerStore = async () => {
+  axios.get('api/v1/partners')
+    .then((response) => {
+      client.set('partners', JSON.stringify(response.data), redis.print);
+    })
+    .catch(error => error.message)
+    .finally(() => client.quit());
+};
+
+// Finds a partner using partnerId
+export const findPartnerById = partnerId => new Promise((resolve, reject) => {
+  client.get('partners', (error, result) => {
+    client.quit();
+    if (error) {
+      reject(error);
+    }
+    resolve(JSON.parse(result).values.filter(partner => partner.id === partnerId));
+  });
+});
+
+
+// fetches placements based on status you passed
+export const fetchPlacementsByStatus = (status, callback) => {
+  axios.get(`api/v1/placements?status=${status}`)
+    .then((response) => {
+      callback(null, response.data.values);
+    })
+    .catch(error => callback(error, []))
+    .finally(() => client.quit());
+};
