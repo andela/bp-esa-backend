911de729080903e0fcabef220853d12ae641d573
anaeze <nzediegwu1@gmail.com>
2018-11-12 17:54:13 +0100

    feat(freckle): assign a developer to a project on freckle
    - set `freckle-api` env variable for making http requests to freckle
    - set `freckle-admin-token` env variable to allow admin priviledge
    - define helper functions for restful response
    - define assignProject method to assign a user to a project on freckle
    - Write tests
    
    [Finishes #160437441]

--- server/modules/freckle/projects.js
+++ server/modules/freckle/projects.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 
 import response from '../../helpers/response';
 
+dotenv.config();
+
 const freckleUrl = 'https://api.letsfreckle.com/v2';
+const freckleToken = process.env.FRECKLE_ADMIN_TOKEN;
 
 export async function getAllProjects() {
   try {
-    const projects = await axios.get(`${freckleUrl}/projects?freckle_token=${process.env.FRECKLE_ADMIN_TOKEN}`);
+    const projects = await axios.get(`${freckleUrl}/projects?freckle_token=${freckleToken}`);
     return projects.data;
   } catch (e) {
     return e.data;
@@ -25,7 +29,7 @@ export const createProject = async (projectName) => {
   try {
     const projects = await getAllProjects();
     if (projects && projects.length > 0 && !verifyExistingProject(projects, projectName)) {
-      await axios.post(`${freckleUrl}/projects?freckle_token=${process.env.FRECKLE_ADMIN_TOKEN}`, {
+      await axios.post(`${freckleUrl}/projects?freckle_token=${freckleToken}`, {
         name: projectName,
       });
       return response(false, `${projectName} project successfully added`);
@@ -34,4 +38,22 @@ export const createProject = async (projectName) => {
   } catch (e) {
     return response(true, `Error occurred creating ${projectName} project`);
   }
+};
+
+/**
+ * @desc Assign a user to a project on freckle
+ *
+ * @param {number} userId - Id of the user to be assigned to a project
+ * @param {array} projectIds - Array of integer projectIds to be assigned to the user.
+ *
+ * @returns {promise} - Axios response for request to assign user to freckle project
+ */
+export const assignProject = (userId, projectIds) => {
+  const url = `${freckleUrl}/users/${userId}/give_access_to_projects?freckle_token=${freckleToken}`;
+  return axios
+    .put(url, {
+      project_ids: projectIds,
+    })
+    .then(() => response(false, 'Successfully added developer to the project'))
+    .catch(error => response(true, error));
 };
