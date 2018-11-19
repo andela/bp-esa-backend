7207b4913645656cd5767eadb56f1a9ddddb1d04
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2018-11-19 11:49:49 +0100

    ch(eslint): enforce the use of JSDoc
    - add JSDoc rule in eslint configuration
    - add JSDoc comments to some functions that are yet to be commented
    - modify the JSDoc comments that are not properly written
    
    [Finishes #161950268]

--- server/helpers/response.js
+++ server/helpers/response.js
@@ -1,3 +1,12 @@
-export default function response(error, message, data) {
-  return data ? { error, message, data } : { error, message };
-}
+/**
+ * @desc - An helper function for sending response.
+ * @param {Boolean} error - A truthy value to tell if there was an error or not.
+ * @param {String} message - The message to send as part of the response.
+ * @param {*} data - The data to send as part of the response.
+ * @returns {Object} The response object.
+ */
+const response = (error, message, data) => (
+  data ? { error, message, data } : { error, message }
+);
+
+export default response;
