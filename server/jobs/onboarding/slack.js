15d3b1c9d7baa84164a8664ecad700aa4b4cb0a3
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2018-12-14 15:35:20 +0100

    ft(slack-automation): add fellow to rack-city slack channel during onboarding
    - add SLACK_RACK_CITY_CHANNEL_ID as a required env var
    - update the slack onboarding job
    
    [Finishes #162664345]

--- server/jobs/onboarding/slack.js
+++ server/jobs/onboarding/slack.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { accessChannel, createPartnerChannel } from '../../modules/slack/slackIntegration';
 
 dotenv.config();
-const { SLACK_AVAILABLE_DEVS_CHANNEL_ID } = process.env;
+const { SLACK_AVAILABLE_DEVS_CHANNEL_ID, SLACK_RACK_CITY_CHANNEL_ID } = process.env;
 
 /**
  * @desc Automates developer onboarding on slack
@@ -17,6 +17,7 @@ const slackOnBoarding = async (placement) => {
   const { client_name: partnerName } = placement;
 
   accessChannel(fellow.email, SLACK_AVAILABLE_DEVS_CHANNEL_ID, 'kick');
+  accessChannel(fellow.email, SLACK_RACK_CITY_CHANNEL_ID, 'invite');
   createPartnerChannel(partnerName, 'internal');
   createPartnerChannel(partnerName, 'general').then((channel) => {
     if (channel.id) {
