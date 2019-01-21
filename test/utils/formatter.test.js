0aca28c0adc76d70fdab0659d21d43ad9208ca03
Parfait Pascal Sibomana <sasipa90@hotmail.com>
2019-01-21 11:28:32 +0300

    add test cases

---
+++ test/utils/formatter.test.js
@@ -0,0 +1,40 @@
+import {
+  getOveralStatus,
+  formatSlackAutomations,
+  formatFreckleAutomations,
+  formatEmailAutomations,
+  formatAutomation,
+  formatAutomationResponse,
+} from '../../server/utils/formatter';
+import { automationsMockData, expectedReponseMockData } from '../mocks/automations';
+
+describe('API Response Payload Formatter', () => {
+  it('should return overall status of the slack automations', () => {
+    expect(getOveralStatus(automationsMockData[0].slackAutomations)).to.be.false;
+  });
+
+  it('should format slack automations payload', () => {
+    const formatedResponse = formatSlackAutomations(automationsMockData[0].slackAutomations);
+    Object.is(formatedResponse, expectedReponseMockData[0].slackAutomations);
+  });
+
+  it('should format freckle automations payload', () => {
+    const formatedResponse = formatFreckleAutomations(automationsMockData[0].freckleAutomations);
+    Object.is(formatedResponse, expectedReponseMockData[0].freckleAutomations);
+  });
+
+  it('should format email automations payload', () => {
+    const formatedResponse = formatEmailAutomations(automationsMockData[0].emailAutomations);
+    Object.is(formatedResponse, expectedReponseMockData[0].emailAutomations);
+  });
+
+  it('should format single automation payload', () => {
+    const formatedResponse = formatAutomation(automationsMockData[0]);
+    Object.is(formatedResponse, expectedReponseMockData[0]);
+  });
+
+  it('should format automations payload', () => {
+    const formatedResponse = formatAutomationResponse(automationsMockData);
+    Object.is(formatedResponse, expectedReponseMockData);
+  });
+});
