5c25b7a228a6de82bcaa009af7781ef313a9fc2f
anaeze <nzediegwu1@gmail.com>
2019-01-23 13:11:28 +0100

    chore(improve-test-coverage): - write test for validateEnvVariable function - write test for custom response function

--- server/validator.js
+++ server/validator.js
@@ -1,27 +1,27 @@
+export const requiredEnvVariables = [
+  'SLACK_TOKEN',
+  'GMAIL_CLIENT_ID',
+  'GMAIL_CLIENT_SECRET',
+  'GMAIL_REFRESH_TOKEN',
+  'GMAIL_REDIRECT_URL',
+  'EMAIL_USER',
+  'OPS_EMAIL',
+  'IT_EMAIL',
+  'ANDELA_ALLOCATIONS_API_TOKEN',
+  'FRECKLE_ADMIN_TOKEN',
+  'SLACK_AVAILABLE_DEVS_CHANNEL_ID',
+  'SLACK_RACK_CITY_CHANNEL_ID',
+  'ANDELA_API_BASE_URL',
+];
+
 const validateEnvironmentVars = () => {
-  const requiredEnvVariables = [
-    'SLACK_TOKEN',
-    'GMAIL_CLIENT_ID',
-    'GMAIL_CLIENT_SECRET',
-    'GMAIL_REFRESH_TOKEN',
-    'GMAIL_REDIRECT_URL',
-    'EMAIL_USER',
-    'OPS_EMAIL',
-    'IT_EMAIL',
-    'ANDELA_ALLOCATIONS_API_TOKEN',
-    'FRECKLE_ADMIN_TOKEN',
-    'SLACK_AVAILABLE_DEVS_CHANNEL_ID',
-    'SLACK_RACK_CITY_CHANNEL_ID',
-    'ANDELA_API_BASE_URL',
-  ];
   const missingVariable = [];
   requiredEnvVariables.forEach((variable) => {
-    if (!process.env[variable]) {
-      missingVariable.push(variable);
-    }
+    if (!process.env[variable]) missingVariable.push(variable);
   });
-  if (missingVariable.length >= 1) {
-    throw Error(`${missingVariable.length} environment variables are missing: ${missingVariable.join(', ')}`);
+  if (missingVariable.length) {
+    const error = `${missingVariable.length} environment variables are missing: ${missingVariable}`;
+    throw Error(error);
   }
 };
 
