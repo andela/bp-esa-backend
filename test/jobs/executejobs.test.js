fb931521d25b1454a0177bdff1b24d5dab0609ef
Valentine Mayaki <mxvmayaki@gmail.com>
2019-04-05 11:46:36 +0100

    fix for timeout undefined

--- test/jobs/executejobs.test.js
+++ test/jobs/executejobs.test.js
@@ -6,10 +6,9 @@ const fakeIoEmit = sinon.spy(io, 'emit');
 
 describe('Jobs Execution Test Suite', async () => {
   it('should execute onboarding jobs successfully', async () => {
-    this.timeout(20 * 1000);
     // get automations from db and test that parent data matches the newPlacements
     await executejobs('onboarding');
     sinon.assert.called(fakeIoEmit);
     fakeIoEmit.restore();
-  });
+  }).timeout(20 * 1000);
 });
