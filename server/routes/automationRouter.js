b71de25f31ba8ddb9700fd1eb8e04345c1e4b90c
machariamarigi <machariamarigi@gmail.com>
2019-04-08 10:37:13 +0300

    Add 'automation/stats' endpoint

--- server/routes/automationRouter.js
+++ server/routes/automationRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import automationController from '../controllers/automationController';
+import automationStatusController from '../controllers/automationStatsController';
 
 const router = express.Router();
 
 router.get('/', automationController.getAutomations);
+router.get('/stats', automationStatusController);
 
 export default router;
