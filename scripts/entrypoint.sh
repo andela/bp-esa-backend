47186770c30e6fb01da5daea8a4b2711d98529c4
william sserubiri <deriwilliams2008@gmail.com>
2019-07-25 15:03:45 +0300

    ch(migrationScript): Add a shell script to run migrations before starting the application - Add a shell script to run migrations before starting the application
    [Starts #167486077]

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+
+printf "\n\n======================================\n"
+printf "Run database migrations"
+printf "\n======================================\n\n"
+yarn migrations
+
+printf "\n\n======================================\n"
+printf "Start the application"
+printf "\n======================================\n\n"
+node ./build/index.js
