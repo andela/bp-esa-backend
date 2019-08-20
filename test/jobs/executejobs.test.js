d4f323f97c251138a85593dbebeac785ea730452
anaeze <nzediegwu1@gmail.com>
2019-08-20 16:30:57 +0100

    - mock slack api calls in job execution tests

--- test/jobs/executejobs.test.js
+++ test/jobs/executejobs.test.js
@@ -1,9 +1,25 @@
 import sinon from 'sinon';
+import proxyquire from 'proxyquire';
 import executejobs from '../../server/jobs';
 import { io } from '../../server';
 import { redisdb } from '../../server/helpers/redis';
+import slackMocks from '../mocks/slack';
+import { slack } from '../../server/modules/slack/slackIntegration';
+import email from '../../server/modules/email/emailTransport';
 
 const fakeIoEmit = sinon.spy(io, 'emit');
+
+// const email = proxyquire('../../server/modules/email/emailModule', {
+//   './emailTransport': { sendMail: () => sinon.stub() },
+// });
+
+const lookupByEmail = sinon.stub(slack, 'lookupByEmail').callsFake(() => slackMocks.slackUser);
+const invite = sinon.stub(slack, 'invite').callsFake(() => slackMocks.inviteUser);
+const kick = sinon.stub(slack, 'kick').callsFake(() => slackMocks.removeUser);
+const create = sinon.stub(slack, 'createChannel').callsFake(() => slackMocks.createGroups);
+const info = sinon.stub(slack, 'channelInfo').callsFake(() => slackMocks.groupInfo);
+const listChannels = sinon.stub(slack, 'listChannels').callsFake(() => slackMocks.channelList);
+const sendMail = sinon.stub(email, 'sendMail').callsFake(() => {});
 
 describe('Jobs Execution Test Suite', async () => {
   it('should execute onboarding jobs successfully', async () => {
@@ -11,6 +27,14 @@ describe('Jobs Execution Test Suite', async () => {
     await redisdb.delete('andela-partners');
     await executejobs('onboarding');
     sinon.assert.called(fakeIoEmit);
+
     fakeIoEmit.restore();
+    lookupByEmail.restore();
+    invite.restore();
+    kick.restore();
+    create.restore();
+    info.restore();
+    listChannels.restore();
+    sendMail.restore();
   }).timeout(30 * 1000);
 });
