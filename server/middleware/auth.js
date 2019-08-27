6bd96732461732cb6b65620c8592aff1a66e5204
ssekito_charles <charles.ssekito@andela.com>
2019-08-27 20:11:47 +0300

    feat(auth):add check for user roles

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,15 +9,24 @@ const authenticateUser = async (req, res, next) => {
   const token = await req.headers.authorization;
   if (!token) return res.status(401).json({ success: false, error: 'No token provided' });
 
-  jwt.verify(
-    token,
-    setJwtKey(process.env.NODE_ENV),
-    (error, decodedToken) => {
-      if (error) return res.status(401).json({ success: false, error: 'Token is not valid' });
+  jwt.verify(token, setJwtKey(process.env.NODE_ENV), (error, decodedToken) => {
+    const userDetails = decodedToken;
+    const { UserInfo } = userDetails || {};
+    const roles = UserInfo ? UserInfo.roles : [];
+    const userRole = process.env.USER_ROLE;
+    const adminRoles = Object.keys(roles).includes(userRole);
+    if (adminRoles) {
+      if (error) return {};
       req.user = decodedToken;
       return next();
-    },
-  );
+    }
+    if (decodedToken === undefined) {
+      return res.status(401).json({ success: false, error: 'Token is not valid' });
+    }
+    return res
+      .status(403)
+      .json({ success: false, error: "the user doesn't have permissions to perform this action" });
+  });
   return false;
 };
 
